70a3c4104d56f0c355bd4faeb0ded3300cd3c511
benjaminshi02003220 <346473509@qq.com>
2018-03-12 14:54:59 +0800

    nn modeling

--- nn_Modeling.py
+++ nn_Modeling.py
@@ -1,18 +1,12 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Mar 11 16:53:09 2018
-
-@author: v-beshi
-"""
 import predict_btc_future
 from sklearn import preprocessing
 from sklearn.decomposition import IncrementalPCA
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
 from sklearn.model_selection import GridSearchCV
+import pickle
 
 data=predict_btc_future.get_agg_data()
-
 data['bids_wall']=data['bfx_bids_wall']/100
 data['asks_wall']=data['bfx_asks_wall']/100
 data['total_bids']=data['bfx_total_bids']/100
@@ -27,9 +21,23 @@ y3=trade_data['next_price15']
 PCA=IncrementalPCA(n_components=5)
 PCA.fit(features)
 PCA_feature=PCA.transform(features)
+
 X_train,X_test,y1_train,y1_test=train_test_split(PCA_feature,y1,test_size=0.33)
-rfr=RandomForestClassifier()
-parameters={'n_estimators':(5,10,15,20),'criterion':('gini','entropy'),'max_depth':(None,2,4)}
-clf=GridSearchCV(rfr,parameters)
+nn=MLPClassifier()
+parameters={'activation':['identity','logistic','tanh','relu']}
+clf=GridSearchCV(nn,parameters)
 clf.fit(X_train,y1_train)
-print(clf.score(X_test,y1_test))
+
+X_train,X_test,y2_train,y2_test=train_test_split(PCA_feature,y2,test_size=0.33)
+nn=MLPClassifier()
+clf2=GridSearchCV(nn,parameters)
+clf2.fit(X_train,y2_train)
+
+X_train,X_test,y3_train,y3_test=train_test_split(PCA_feature,y3,test_size=0.33)
+nn=MLPClassifier()
+clf3=GridSearchCV(nn,parameters)
+clf3.fit(X_train,y3_train)
+
+s_next5=pickle.dumps(clf)
+s_next10=pickle.dumps(clf2)
+s_next15=pickle.dumps(clf3)
