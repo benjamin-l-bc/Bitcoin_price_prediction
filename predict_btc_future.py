02cf0b7a0748bef519b614b559b4c51e562a682a
benjaminshi02003220 <346473509@qq.com>
2018-03-14 23:25:52 +0800

    Delete Feature

--- predict_btc_future.py
+++ predict_btc_future.py
@@ -78,12 +78,6 @@ def get_agg_data():
         pre_news10.append((raw_data['news_emotion'][i]-raw_data['news_emotion'][i-10])/(raw_data['news_emotion'][i-10]))
     pre_news10=pd.Series(pre_news10,name='pre_news10')
 
-    pre_8btc10=[]
-    for i in range(0,10):
-        pre_8btc10.append(0)
-    for i in range(10,len(raw_data)):
-        pre_8btc10.append((raw_data['8btc_emotion'][i]-raw_data['8btc_emotion'][i-10])/(raw_data['8btc_emotion'][i-10]))
-    pre_8btc10=pd.Series(pre_8btc10,name='pre_8btc10')
     
     raw_data['bids_wall']=raw_data['bfx_bids_wall']/100
     raw_data['asks_wall']=raw_data['bfx_asks_wall']/100
@@ -92,7 +86,7 @@ def get_agg_data():
     raw_data['buy_volumn']=raw_data['bfx_buy_volumn']/50
     raw_data['sell_volumn']=raw_data['bfx_sell_volumn']/50
 
-    raw_data=raw_data.drop(['ok0330','DateTime','ok_thisweek','huobi_USDT','exchange_rate','bfx_last_price','news_emotion','8btc_emotion','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn'],axis=1)
-    agg_data=pd.concat([raw_data,pre_price15,pre_price10,pre_price5,pre_bfx,pre_news10,pre_8btc10,next_price5,next_price10,next_price15],axis=1)
+    raw_data=raw_data.drop(['ok0330','DateTime','ok_thisweek','huobi_USDT','exchange_rate','bfx_last_price','news_emotion','bfx_bids_wall','bfx_asks_wall','bfx_total_bids','bfx_total_asks','bfx_buy_volumn','bfx_sell_volumn'],axis=1)
+    agg_data=pd.concat([raw_data,pre_price15,pre_price10,pre_price5,pre_bfx,pre_news10,next_price5,next_price10,next_price15],axis=1)
     agg_data=agg_data[15:len(agg_data)-15]
     return(agg_data)
